5f9d1b39e8b9aad80f1d7174960038ba54a1b6a6
ggooltae <waterboy94@naver.com>
2022-11-14 21:09:42 +0900

    feat: verifyToken 메세지 변경

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -10,7 +10,7 @@ async function verifyToken(req, res, next) {
     next();
   } catch (error) {
     error.status = statusCodes.UNAUTHORIZED;
-    error.message = "unauthorized";
+    error.message = "사용자 정보가 유효하지 않습니다. 다시 로그인해주세요.";
 
     next(error);
   }
