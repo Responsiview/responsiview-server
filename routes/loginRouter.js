cb7182e761db8bb5453b78d06afbf81d91b29665
ggooltae <waterboy94@naver.com>
2022-11-20 01:10:43 +0900

    refactor: 로그인 중 jwt 생성과정 에러핸들링 추가

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -7,16 +7,20 @@ const router = express.Router();
 router
   .route("/")
   .post(loginRouterController.checkUserOrCreate, (req, res, next) => {
-    const token = jwt.sign(
-      { userEmail: req.body.userEmail },
-      process.env.JWT_SECRET_KEY,
-      { expiresIn: "1h" },
-    );
+    try {
+      const token = jwt.sign(
+        { userEmail: req.body.userEmail },
+        process.env.JWT_SECRET_KEY,
+        { expiresIn: "1h" },
+      );
 
-    res.cookie("token", token, { httpOnly: true });
-    res.cookie("userEmail", req.body.userEmail);
+      res.cookie("token", token, { httpOnly: true });
+      res.cookie("userEmail", req.body.userEmail);
 
-    res.json({ result: "ok", userEmail: req.body.userEmail });
+      res.json({ result: "ok", userEmail: req.body.userEmail });
+    } catch (error) {
+      next(error);
+    }
   });
 
 module.exports = router;
