0e9f80fd25d4be94abe299f52a37b8ab1a44381f
ggooltae <waterboy94@naver.com>
2022-11-25 15:05:51 +0900

    refactor: 로그아웃 라우터 삭제

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 const loginRouter = require("./routes/loginRouter");
-const logoutRouter = require("./routes/logoutRouter");
 const presetRouter = require("./routes/presetRouter");
 
 const app = express();
@@ -25,7 +24,6 @@ app.use(
 );
 
 app.use("/login", loginRouter);
-app.use("/logout", logoutRouter);
 app.use("/user/:userEmail/preset", presetRouter);
 
 app.use(function (err, req, res, next) {
