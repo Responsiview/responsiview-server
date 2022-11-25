f73ad33f3e3e516c379f650180dc6f80aa4d77fb
ggooltae <waterboy94@naver.com>
2022-11-25 18:35:41 +0900

    chore: eb deploy 세팅

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [process.env.BASE_CLIENT_URL],
+    origin: "*",
     credentials: true,
   }),
 );
