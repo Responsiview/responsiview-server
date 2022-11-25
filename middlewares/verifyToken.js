1952688f94bfcf28eebeaf774e5d60b8e919946f
ggooltae <waterboy94@naver.com>
2022-11-25 15:07:09 +0900

    refactor: 사용자인증시 header의 token을 가져오는방식으로 변경

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -2,16 +2,13 @@ const jwt = require("jsonwebtoken");
 const statusCodes = require("http-status-codes");
 
 async function verifyToken(req, res, next) {
-  const clientToken = req.cookies.token;
+  const clientToken = req.headers.authorization.replace("Bearer ", "");
 
   try {
     await jwt.verify(clientToken, process.env.JWT_SECRET_KEY);
 
     next();
   } catch (error) {
-    res.clearCookie("token");
-    res.clearCookie("userEmail");
-
     error.status = statusCodes.UNAUTHORIZED;
     error.message = "사용자 정보가 유효하지 않습니다. 다시 로그인해주세요.";
 
